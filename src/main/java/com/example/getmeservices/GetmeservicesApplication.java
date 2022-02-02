32cbfca36907b6f13152d72bff1d2ad7b040ebe8
kevinkapoor454 <73305888+kevinkapoor454@users.noreply.github.com>
2022-02-02 13:01:08 -0500

    first commit

---
+++ src/main/java/com/example/getmeservices/GetmeservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.getmeservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetmeservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetmeservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/getmeservices/GetmeservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getmeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetmeservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
