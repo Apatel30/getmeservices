c5438ed7546d046fb7efbdda4fe493a64a66133f
kevinkapoor454 <73305888+kevinkapoor454@users.noreply.github.com>
2022-02-07 12:01:19 -0500

    S3 Add 4 Apis

---
+++ src/main/java/com/example/getmeservices/service/FileService.java
@@ -0,0 +1,45 @@
+package com.example.getmeservices.service;
+
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FileService {
+    public boolean upload(MultipartFile file) {
+        BasicAWSCredentials credentials= new BasicAWSCredentials("AKIATJKPHITO5D4RXMTA", "W+5GvdNaHRXQTGFKvFZsLUde2vL0t81qQmwc+Yua");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.CA_CENTRAL_1).build();
+        try {
+
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+            s3.putObject("backend22", file.getOriginalFilename(), file.getInputStream(), metadata);
+            return true;
+        } catch (AmazonServiceException | IOException e) {
+           e.printStackTrace();
+            return false;
+        }
+    }
+    public S3Object getFile(String key){
+        BasicAWSCredentials credentials= new BasicAWSCredentials("AKIATJKPHITO5D4RXMTA", "W+5GvdNaHRXQTGFKvFZsLUde2vL0t81qQmwc+Yua");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.CA_CENTRAL_1).build();
+        return s3.getObject("backend22", key);
+    }
+    public void deleteFile(String key){
+        BasicAWSCredentials credentials= new BasicAWSCredentials("AKIATJKPHITO5D4RXMTA", "W+5GvdNaHRXQTGFKvFZsLUde2vL0t81qQmwc+Yua");
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.CA_CENTRAL_1).build();
+        s3.deleteObject("backend22", key);
+    }
+}
